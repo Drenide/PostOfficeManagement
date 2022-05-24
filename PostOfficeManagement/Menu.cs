b5f671e1682c3b182968341579b737a1400bcdf9
Drenide <101639615+Drenide@users.noreply.github.com>
2022-05-24 02:40:09 +0200

    Add files via upload

--- PostOfficeManagement/Menu.cs
+++ PostOfficeManagement/Menu.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace PostOfficeManagement
@@ -19,12 +20,7 @@ namespace PostOfficeManagement
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            DialogResult dr = MessageBox.Show("Jeni i sigurt se deshironi te largoheni?", "Dalja nga programi", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
-
-            if (dr == DialogResult.Yes)
-            {
-                Application.Exit();
-            }
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -41,20 +37,64 @@ namespace PostOfficeManagement
             SidePanel.Height = button2.Height;
             SidePanel.Top = button2.Top;
             this.Hide();
-            RegjistrimiPorosise b = new RegjistrimiPorosise();
-            b.Show();
+            Porosite p = new Porosite();
+            p.Show();
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
             SidePanel.Height = button3.Height;
             SidePanel.Top = button3.Top;
+            this.Hide();
+            RegjistrimiPorosise rp = new RegjistrimiPorosise();
+            rp.Show();
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            SidePanel.Height = button4.Height;
-            SidePanel.Top = button4.Top;
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Jeni i sigurt se deshironi te largoheni?", "Dalja nga programi", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if (dr == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Jeni i sigurt se deshironi te largoheni nga Menu dhe te ktheheni te pjesa e Login?", "Dalja nga programi", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if (dr == DialogResult.Yes)
+            {
+                this.Hide();
+                Login ss = new Login();
+                ss.Show();
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://www.facebook.com/");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://www.instagram.com/");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://www.linkedin.com/");
         }
     }
 }
